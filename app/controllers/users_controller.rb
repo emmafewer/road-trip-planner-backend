9d6941f37647cf3e94d0fca07c559534ff2a8b16
emmafewer <emmafewer20@gmail.com>
2021-02-25 14:52:43 -0800

    wheewww

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,35 @@
 class UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create, :index]
+    
+    def index
+        users = User.all 
+        render json: users
+    end
+
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            my_token = encode_token({user_id: user.id})
+            render json: {id: user.id, username: user.username, name: user.name, token: my_token}
+        else
+            render json: {error: 'Failed to create user'}, status: :not_acceptable
+        end
+    end
+
+    def update
+        user = User.find(params[:id])
+        user.name = params[:name]
+        render :json => user
+    end 
+
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
   resources :parks
   resources :road_trips
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post '/signup', to: 'users#create'
+  post '/login', to: 'auth#create'
+  get '/persist', to: 'auth#show'
+
 end
