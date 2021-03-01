ea6b679d5554dceddccad92f74eb97e66ff91e0b
emmafewer <emmafewer20@gmail.com>
2021-03-01 10:46:15 -0800

    road trip show

--- app/controllers/road_trips_controller.rb
+++ app/controllers/road_trips_controller.rb
@@ -1,4 +1,9 @@
 class RoadTripsController < ApplicationController
+    
+    def show
+        road_trip = RoadTrip.find(params[:id])
+        render json: road_trip, include: [:parks]
+    end
 
     def create
         road_trip = RoadTrip.create(name: road_trip_params['name'], user_id: road_trip_params['user_id'])
